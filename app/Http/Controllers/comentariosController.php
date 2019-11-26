f7c42c91d58f1333f8a5eb57b72a9c9146995f22
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-11-26 08:10:23 +0100

    la insercion de comentarios funciona correctamente

--- app/Http/Controllers/comentariosController.php
+++ app/Http/Controllers/comentariosController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 use App\Mensajes;
+use Illuminate\Http\Request;
 
 class comentariosController extends Controller
 {
-    function insertarComentario(requests $request)
+    function insertarComentario(request $request)
     {
         $mensaje = new Mensajes;
-            $mensaje->name = $request->input('Nombre');
+            $mensaje->nombre = $request->input('Nombre');
             $mensaje->email = $request->input('Email');
             $mensaje->comentario = $request->input('Comentario');
         $mensaje->save();
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/comentario','comentariosController@insertarComentario')->name('insertarComentario');
+Route::post('/','comentariosController@insertarComentario')->name('insertarComentario');
